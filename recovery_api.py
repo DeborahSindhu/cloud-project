d355731c286ace4973dcedaa815f5753a92e1ab2
DeborahSindhu <167852536+DeborahSindhu@users.noreply.github.com>
2025-05-29 10:03:10 +0530

    Update recovery_api.py

--- recovery_api.py
+++ recovery_api.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Flask API is running! Available routes: /restore/<bucket_name>/<file_name>"}), 200
+    return jsonify({"message": "Flask API is running! Available routes: /restore/sindhu_test/sample.txt"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
